1377ab4d24ea4b1571aab5d8b3b1a5bc8252d965
Gomathi Nachiyar <gomathinachiyar.u@gmail.com>
2025-08-06 20:34:42 +0530

    Fix_error

--- bms-ui/src/app/components/login/login.component.ts
+++ bms-ui/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { UserService } from 'src/app/services/user.service';
+import { UserService } from '../../services/user.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
